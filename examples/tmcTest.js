c767eb666505cd25df072e83df8a80292cd12935
xiamencloud <xiamencloud@gmail.com>
2017-06-02 16:45:39 +0800

    modified:   examples/httpServer.js 	modified:   examples/tmcTest.js 	modified:   lib/api/network.js 	modified:   lib/spiUtil.js

--- examples/tmcTest.js
+++ examples/tmcTest.js
@@ -5,8 +5,8 @@ var tmcClient = new TmcClient('23021137','6c4522b216ad2314477ae33616d9e1f9','def
 tmcClient.connect('ws://mc.api.taobao.com/',
     function (message,confirmCb) {
         console.log(message);
-	    //如果处理失败,不用调用confirmCb
+        //如果处理失败,不用调用confirmCb
 
-	    //如果处理成功
+        //如果处理成功
         confirmCb();
     });
